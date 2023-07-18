fa422b544eca7c3f191ae40d8e3e54a971fb5975
Halima-Ikuomola <125281365+Halima-Ikuomola@users.noreply.github.com>
2023-07-18 20:16:27 +0100

    Conditional back links added for august release

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,29 @@
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
 })
+
+const express = require('express');
+const nunjucks = require('nunjucks');
+
+const app = express();
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+// Middleware to handle 'show_back_link' query parameter for all routes
+app.use((req, res, next) => {
+  res.locals.showBackLink = req.query.show_back_link === 'true';
+  next();
+});
+
+// Route handler for the specific route '/development-programmes'
+app.get('/development-programmes', (req, res) => {
+  res.render('development-programmes.njk');
+});
+
+// Add more route handlers for other pages as needed
+
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
+});
